6e6ced22fd626aa8806626e0d0c4e75d8bd1f18b
Rodrigo David Oliveira <digo.david.oliveira@gmail.com>
2019-04-17 21:56:21 -0300

    Boas práticas Python

--- crypto.py
+++ crypto.py
@@ -1,12 +1,8 @@
 import random
 import math
 
-msg = ""
-publicKey = ""
-privateKey = ""
-
 def criptografia():
-    #msg = input('Insira o texto para criptografar: ')
+    '''msg = input('Insira o texto para criptografar: ')'''
     bits = int(input('Insira a quantidade de bits mínimo: '))
     print("Gerando Chaves")
     p = generateLargePrime(bits)
@@ -15,12 +11,14 @@ def criptografia():
     print("q = ", q)
     generateKeys(p, q, bits)
 
+
 def generateLargePrime(keysize=512):
-    # Return a random prime number of keysize bits in size.
+    '''Return a random prime number of keysize bits in size.'''
     while True:
         num = random.randrange(2**(keysize-1), 2**(keysize))
         if isPrime(num):
             return num
+
 
 def generateKeys(p,q,bits):
     n = p*q
@@ -33,17 +31,18 @@ def generateKeys(p,q,bits):
             break
     print("e: ",e)
 
-
     d = findModInverse(e, (p - 1) * (q - 1))
     publicKey = (n, e)
     privateKey = (n, d)
     print('Public key:', publicKey)
     print('Private key:', privateKey)
 
+
 def gcd(a, b):
     while a != 0:
         a, b = b % a, a
     return b
+
 
 def findModInverse(a, m):
     if gcd(a, m) != 1:
@@ -56,19 +55,12 @@ def findModInverse(a, m):
         v1, v2, v3, u1, u2, u3 = (u1 - q * v1), (u2 - q * v2), (u3 - q * v3), v1, v2, v3
     return u1 % m
 
+
 def isPrime(num):
-    # Return True if num is a prime number. This function does a quicker
-    # prime number check before calling rabinMiller().
-
-
+    '''Return True if num is a prime number. This function does a quicker
+    prime number check before calling rabinMiller().'''
     if (num < 2):
         return False # 0, 1, and negative numbers are not prime
-
-
-    # About 1/3 of the time we can quickly determine if num is not prime
-    # by dividing by the first few dozen prime numbers. This is quicker
-    # than rabinMiller(), but unlike rabinMiller() is not guaranteed to
-    # prove that a number is prime.
     lowPrimes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, \
     79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, \
     181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, \
@@ -79,35 +71,31 @@ def isPrime(num):
     821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, \
     953, 967, 971, 977, 983, 991, 997]
 
-
     if num in lowPrimes:
         return True
-
-
     # See if any of the low prime numbers can divide num
     for prime in lowPrimes:
         if (num % prime == 0):
             return False
 
-
     # If all else fails, call rabinMiller() to determine if num is a prime.
     return rabinMiller(num)
 
+
 def rabinMiller(num):
-    # Returns True if num is a prime number.
+    '''Retorna True se num é primo'''
     s = num - 1
     t = 0
     while s % 2 == 0:
-        # keep halving s while it is even (and use t
-        # to count how many times we halve s)
+        '''keep halving s while it is even (and use t
+        to count how many times we halve s)'''
         s = s // 2
         t += 1
 
-
-    for trials in range(5): # try to falsify num's primality 5 times
+    for trials in range(5):
         a = random.randrange(2, num - 1)
         v = pow(a, s, num)
-        if v != 1: # this test does not apply if v is 1.
+        if v != 1: #nao se aplica se v eh 1
             i = 0
             while v != (num - 1):
                 if i == t - 1:
@@ -117,4 +105,6 @@ def rabinMiller(num):
                     v = (v ** 2) % num
     return True
 
-criptografia()
+
+if __name__ == '__main__':
+    criptografia()
